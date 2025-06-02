71a93bf6b785d1e3ef04a95c7ca4d72c5f8b4bda
Shubham <shubhamtanwar72@gmail.com>
2025-06-03 01:51:18 +0530

    initial commit

--- dice-rolling-game/dice-rolling-game.py
+++ dice-rolling-game/dice-rolling-game.py
@@ -13,17 +13,48 @@ import random
 #         print("Invalid respones!")
 
 # guess the random number b/w 1-10
-random = random.randint(1,10)
-print(random)
-while True:
-    try:
-        guess=int(input("guess the number : "))
-        if guess<random:
-            print("too low")
-        elif guess>random:
-            print("too high")
-        else:
-            print("congrats you guessed the number : ", random)
-            break
-    except:
-        print("please type a number")
+# random = random.randint(1,10)
+# print(random)
+# while True:
+#     try:
+#         guess=int(input("guess the number : "))
+#         if guess<random:
+#             print("too low")
+#         elif guess>random:
+#             print("too high")
+#         else:
+#             print("congrats you guessed the number : ", random)
+#             break
+#     except:
+#         print("please type a number")
+
+# rolling dice game
+
+pick = input("pick rock-paper or scissors : (r/p/c) - ")
+
+comp = random.choice(["r", "p", "c"])
+
+if pick == "r":
+    if comp == "p":
+        print("you lost computer chose : ", comp)
+    elif comp == "r":
+        print("Its a draw computer chose : ", comp)
+    elif comp == "c":
+        print("Congrats you won computer chose : ", comp)
+
+elif pick == "p":
+    if comp == "p":
+        print("Its a draw computer chose : ", comp)
+    elif comp == "r":
+        print("Congrats you won computer chose : ", comp)
+    elif comp == "c":
+        print("you lost computer chose : ", comp)
+
+elif pick == "c":
+    if comp == "p":
+        print("Congrats you won computer chose : ", comp)
+    elif comp == "r":
+        print("you lost computer chose : ", comp)
+    elif comp == "c":
+        print("Its a draw computer chose : ", comp)
+
